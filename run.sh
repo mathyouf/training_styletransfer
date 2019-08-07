5e1fa7c216dd593a31c78a56fad900444e46d5a3
Matthew Allen Fisher <11fishem@gmail.com>
2019-08-06 20:05:24 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,4 @@
-#! /bin/bash
 
-APT_PACKAGES="apt-utils ffmpeg libav-tools x264 x265"
-apt-install() {
-	export DEBIAN_FRONTEND=noninteractive
-	apt-get update -q
-	apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $APT_PACKAGES
-	return $?
-}
-
-#install ffmpeg to container
-add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
-apt-install
 bash setup.sh
 pip install -r requirements.txt
 export PYTHONUNBUFFERED=0
